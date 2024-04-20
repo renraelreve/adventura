855f618decf036c84b3adbb05b5b1137224a268c
renraelreve <123564916+renraelreve@users.noreply.github.com>
2024-04-20 23:35:43 +0700

    commit

--- src/api/favouritesApi.js
+++ src/api/favouritesApi.js
@@ -2,6 +2,7 @@
 
 import axios from "axios";
 
-const BASE_URL = "https://661d0b3fe7b95ad7fa6bf345.mockapi.io/adventura/api/v1/";
+const BASE_URL =
+  "https://cors-anywhere.herokuapp.com/661d0b3fe7b95ad7fa6bf345.mockapi.io/adventura/api/v1/";
 
 export const favouritesApi = axios.create({ baseURL: BASE_URL });
