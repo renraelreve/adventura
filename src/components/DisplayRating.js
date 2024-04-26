4c6a1a5916c4699c2a60eacad68bc05e35107878
Ng Teck Guan <renraelreve@gmail.com>
2024-04-27 07:55:49 +0800

    added placeholder

--- src/components/DisplayRating.js
+++ src/components/DisplayRating.js
@@ -1,14 +1,15 @@
-import Box from '@mui/material/Box';
-import Rating from '@mui/material/Rating';
-import Typography from '@mui/material/Typography';
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Rating from "@mui/material/Rating";
+import Typography from "@mui/material/Typography";
 
-export default function DisplayRating( { value } ) {
+export default function DisplayRating({ value }) {
   // const [value, setValue] = useState(2);
-  
+
   return (
     <Box
       sx={{
-        '& > legend': { mt: 2 },
+        "& > legend": { mt: 2 },
       }}
     >
       {/* <Typography component="legend">Controlled</Typography>
